341a06fcabc65ffece776b10f03c17345be16bbf
unknown <gustavolama20@gmail.com>
2020-03-05 04:57:50 +0200

    Added useragent data

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // One nighter coding challenge the grabify clone
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const shortid = require('shortid');
 const moment = require('moment');
-const http = require('http');
 
 const config = require('./config.json');
 const func = require('./functions/functions.js');
@@ -12,6 +12,7 @@ const func = require('./functions/functions.js');
 let connection = mysql.createConnection(config.database);
 let app = express();
 
+app.use(require('express-useragent').express());
 app.set('view engine', 'pug');
 
 connection.query('CREATE DATABASE IF NOT EXISTS lean_tracker', (err) => {
@@ -44,7 +45,7 @@ app.get('/:id', (req, res) => {
       let ip_address = '141.237.9.209';
       let unique_id = shortid.generate();
       let timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
-      let useragent = req.headers['user-agent'];
+      let user_agent = req.useragent.source;
 
       http.get(`http://ip-api.com/json/${ip_address}`, (response) => {
         let data = '';
@@ -62,9 +63,11 @@ app.get('/:id', (req, res) => {
             ip_address,
             time_captured,
             country,
-            region,
             city,
             timezone,
+            browser,
+            os,
+            platform,
             coordinates,
             isp,
             asp,
@@ -75,13 +78,15 @@ app.get('/:id', (req, res) => {
             '${ip_address}',
             '${timestamp}',
             '${data.country}',
-            '${data.region}',
             '${data.city}',
             '${data.timezone}',
+            '${req.useragent.browser + " (" + req.useragent.version + ")"}',
+            '${req.useragent.os}',
+            '${req.useragent.platform}',
             '${data.lat + ", " + data.lon}',
             '${data.isp}',
             '${data.as}',
-            '${useragent}'
+            '${user_agent}'
           )`;
 
           let original_url = results[0].original_url;
@@ -148,6 +153,58 @@ app.get('/track/:id', (req, res) => {
     }
   });
 });
+
+app.get('/delete/:id', (req, res) => {
+  let id = req.params.id;
+  let sql = `SELECT * FROM tracker WHERE admin_id = '${id}'`;
+
+  connection.query(sql, (err, results, fields) => {
+    if (results.length > 0) {
+      let ip_id = results[0].id;
+      sql = `DELETE FROM ip WHERE id = '${ip_id}'`;
+
+      connection.query(sql, (err, results, fields) => {
+        sql = `DELETE FROM tracker WHERE admin_id = '${id}'`;
+        connection.query(sql, (err, results, fields) => {
+          res.render('alert', {alert: `Successfully deleted tracker with ID: ${id} ✔️`});
+        });
+      });
+
+    } else {
+      res.render('alert', {alert: `Tracker with ID: ${id} not found ❌`});
+    }
+  });
+});
+
+app.get('/change/:id', (req, res) => {
+  let original_url = req.query.url;
+  let id = req.params.id;
+
+  if (original_url) {
+    if (func.validateUrl(original_url)) {
+      let sql = `SELECT * FROM tracker WHERE admin_id = '${id}'`;
+
+      connection.query(sql, (err, results, fields) => {
+        tracking_url = results[0].tracking_url;
+        if (results.length > 0) {
+          sql = `UPDATE tracker SET original_url = '${original_url}' WHERE admin_id = '${id}'`;
+
+          connection.query(sql, (err, results, fields) => {
+            res.redirect(tracking_url);
+          });
+
+        } else {
+          res.render('alert', {alert: `Tracker with ID: ${id} not found ❌`});
+        }
+      });
+    } else {
+      res.render('alert', {alert: 'Input not validated'});
+    }
+  } else {
+    res.redirect('/');
+  }
+});
+
 
 app.post('/create', (req, res) => {
   let original_url = req.query.url;
